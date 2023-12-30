3552c4ff19f37499ca1baf68dd648c4a86b560c2
Isaac Obeng <139678869+isaacobenzy@users.noreply.github.com>
2023-12-30 09:49:18 +0000

    Delete Project directory

--- Project/script.js
+++
@@ -1,5 +0,0 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-
-darkModeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode', darkModeToggle.checked);
-});
